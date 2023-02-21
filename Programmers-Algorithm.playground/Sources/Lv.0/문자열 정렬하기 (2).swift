05317ab5a550c833ac8365b9043694b42febd4a7
jeongsoohee <dkssudgo365@naver.com>
2023-02-21 19:30:20 +0900

    [Programmers] : Lv.0_문자열 정렬하기(2) map 이용해보기

--- Programmers-Algorithm.playground/Sources/Lv.0/문자열 정렬하기 (2).swift
+++ Programmers-Algorithm.playground/Sources/Lv.0/문자열 정렬하기 (2).swift
@@ -37,3 +37,31 @@ func solution1(_ my_string:String) -> String {
     return result
 }
 
+func solution2(_ my_string:String) -> String {
+    
+    //반환할 변수
+    var result: String = ""
+    
+    
+    //매개변수를 소문자로 변환시켜 담을 변수
+    let myString: String = my_string.lowercased()
+    //문자열을 오름차순으로 정렬하여 반환할 변수에 담는다.
+    result += myString.sorted()
+   
+    return result
+}
+
+// map을 이용해보기
+func solution3(_ my_string:String) -> String {
+    //반환할 변수
+    var result: String = ""
+    //스위프트 문법에맞는 카멜케이스형식의 변수
+    let myString: String = my_string
+    // lowercased() 문자열 반환
+    // sorted() 캐릭터 배열 반환
+    // map을 이용하여 String 배열로 변환
+    // .joined()을 이용하여 배열을 평면화
+    result = myString.lowercased().sorted().map{String($0)}.joined()
+    
+    return result
+}
