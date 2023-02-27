f5c2e94d6707c316335dc46f647057afc396bb47
jeongsoohee <dkssudgo365@naver.com>
2023-02-27 20:52:52 +0900

    [Programmers] : Lv.0_숫자 찾기_.enumerated()함수 사용 예시 추가

--- Programmers-Algorithm.playground/Sources/Lv.0/숫자 찾기.swift
+++ Programmers-Algorithm.playground/Sources/Lv.0/숫자 찾기.swift
@@ -28,3 +28,18 @@ private func solution(_ num:Int, _ k:Int) -> Int {
     }
     return -1
 }
+
+
+private func solution1(_ num:Int, _ k:Int) -> Int {
+    //.enumerated()함수를 사용해 인덱스와 값을 모두 얻기
+    for (index,value) in String(num).enumerated() {
+        //String를 하나하나 꺼내게되면 Character타입이 되기때문에 타입 변환 해주기
+        //k도 타입을 비교해보기 위해서 같은 String타입으로 변환하기
+        if String(value) == String(k) {
+            //만약 값이 같다면 인덱스는 자리보다 1이 작기때문에 +1을해주고 리턴하기
+            return index + 1
+        }
+    }
+    //위에 값을 모두 for문으로 돌렸을 때 k와 같은 값이 나오지않는다면 -1을 리턴하기
+    return -1
+}
