11699a82a082a39ff3e9202124825b8d96b1dcd8
jeongsoohee <dkssudgo365@naver.com>
2023-03-06 23:09:45 +0900

    [Programmers] : Lv.0_369게임

---
+++ Programmers-Algorithm.playground/Sources/Lv.0/369게임.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+/* 문제
+ 
+    머쓱이는 친구들과 369게임을 하고 있습니다. 369게임은 1부터 숫자를 하나씩 대며 3, 6, 9가 들어가는 숫자는 숫자 대신 3, 6, 9의 개수만큼 박수를 치는 게임입니다. 머쓱이가 말해야하는 숫자 order가 매개변수로 주어질 때, 머쓱이가 쳐야할 박수 횟수를 return 하도록 solution 함수를 완성해보세요.
+
+ */
+
+/* 제한사항
+ 
+    1 ≤ order ≤ 1,000,000
+ 
+ */
+
+private func solution(_ order:Int) -> Int {
+    //반환할 변수 생성
+    var result: Int = 0
+    //order를 String로 변환 후 filter로 "3","6","9"가 있는 것 들만
+    //반환한 후 count를 세기
+    result = String(order).filter{ $0 == "3" || $0 == "6" || $0 == "9"}.count
+
+    return result
+}
