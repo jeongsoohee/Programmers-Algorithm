d845f81a7472d21a9da0a499f52c92aeed35d6fb
jeongsoohee <dkssudgo365@naver.com>
2023-03-08 18:08:37 +0900

    [Programmers] : Lv.0_k의 개수_고차함수 사용 예시 추가

--- Programmers-Algorithm.playground/Sources/Lv.0/k의 개수.swift
+++ Programmers-Algorithm.playground/Sources/Lv.0/k의 개수.swift
@@ -30,3 +30,22 @@ private func solution(_ i:Int, _ j:Int, _ k:Int) -> Int {
     }
     return count
 }
+
+private func solution1(_ i:Int, _ j:Int, _ k:Int) -> Int {
+    //반환할 변수 생성
+    var result: Int = 0
+    //i...j까지 for문 돌리기
+    for num in i...j { // 1,2,3...10,11,12,13
+        //정수를 문자열로 변환 후 //"1"..."10","11"
+       result += String(num)
+            //String(num)에서 문자 하나하나를 문자열로 변환하고 고차함수 map을 이용해  배열안에 각 요소를 저장
+            .map{ String($0) }  //["1"], ["1","0"]
+            // 각 요소를 구분해서 만들어놓은 map배열에서 하나씩 꺼낸 것을
+            // filter고차함수를 이용해 k와 같은 문자열만 남긴 배열만 리턴하게되면
+            //i부터 j까지 k가 포함되어있는 것만 count를 세서 더해주게 된다!
+            .filter{ $0 == String(k) }.count
+        
+    }
+    return result
+    
+}
